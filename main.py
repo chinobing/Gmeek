5b682301a35a0c7c2081f825e5d61dbfd4a7ed65
Bing Wong <wbbady@outlook.com>
2024-10-29 15:32:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from github import Github
 from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """## [Blog](https://chinobing.github.io/)
-My personal blog([About Me](https://github.com/chinobing/chinobing.github.io/issues/3)) using issues and GitHub Actions (随意转载，无需署名)
+MD_HEAD = """
 ![image](https://github.com/user-attachments/assets/a168bf11-661e-4566-b042-7fc9544de528)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
